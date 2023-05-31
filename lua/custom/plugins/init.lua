37d66583224b5d114f75778cb1f0af6ba03ff11a
Cameron McKay <cameron@webnx.com>
2023-05-30 18:06:11 -0600

    first commit

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,21 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+-- disable netrw at the very start of your init.lua
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {
+      view = {
+        width = 50,
+      }
+    }
+  end,
+}
+
+
