5a2fe63e9d4c71b5be528d6d5a474043d37b720d
Cameron <cameron@webnx.com>
2024-06-04 11:03:24 -0600

    Updated config

--- lua/custom/plugins/transparent.lua
+++ lua/custom/plugins/transparent.lua
@@ -1,1 +1,19 @@
-return {'xiyaowong/transparent.nvim'}
+return {
+  'xiyaowong/transparent.nvim',
+  lazy = false,
+  config = function ()
+    require('transparent').setup({
+      groups = { -- table: default groups
+        'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+        'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+        'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+        'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+      },
+      extra_groups = {
+        "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+        "NvimTreeNormal" -- NvimTree
+      }, -- table: additional groups that should be cleared
+      exclude_groups = {}, -- table: groups you don't want to clear
+    })
+  end
+}
